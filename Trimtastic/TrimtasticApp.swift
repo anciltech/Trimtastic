2c2530ced81fa4e3f4a724187ca5fe8e48032a6d
Ancil Hoffman <anciltech@gmail.com>
2023-04-19 14:20:46 +0200

    Initial Commit

---
+++ Trimtastic/TrimtasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimtasticApp.swift
+//  Trimtastic
+//
+//  Created by Ancil Maxwell Hoffman on 19.04.23.
+//
+
+import SwiftUI
+
+@main
+struct TrimtasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
